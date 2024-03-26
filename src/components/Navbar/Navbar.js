d031dfd761cc52f4f922701726e4180231bc2dc6
Batuhan Kayahan <82649079+batuhanlog@users.noreply.github.com>
2024-03-26 07:53:55 +0300

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -22,8 +22,8 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/doctors">
-                Doktorlar
+              <Link className="nav-link text-white" to="/doctors">
+                Doktorlar👨‍⚕️
               </Link>
             </li>
             <li className="nav-item">
