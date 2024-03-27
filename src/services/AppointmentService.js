9c78931a9a0a762e92af63dd09ac291cb37baa71
Batuhan Kayahan <82649079+batuhanlog@users.noreply.github.com>
2024-03-27 23:30:58 +0300

    Update AppointmentService.js

--- src/services/AppointmentService.js
+++ src/services/AppointmentService.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://vet-backend-3gew.onrender.com/api/v1/appointmentDate";
-
+//
 export const AppointmentService = {
     getAllAppointments: async () => {
         const response = await fetch(`${BASE_URL}`);
